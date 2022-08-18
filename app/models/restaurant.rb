e3052da709034591e0360f95c14bff80b318aeda
Alfred Tay <twx.alfred@gmail.com>
2022-08-18 12:35:37 +0800

    validations are done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :category, presence: true, if: :include_in_category?
-
-  def include_in_category?
-    %w[chinese italian japanese french belgian].include? category
-  end
+  validates :name, :address, presence: true
+  validates :category, presence: true, inclusion: {
+    in: %w[chinese italian japanese french belgian],
+    message: '%<value> is not a valid size'
+  }
 end
--- db/migrate/20220818030136_create_restaurants.rb
+++ db/migrate/20220818030136_create_restaurants.rb
@@ -2,8 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.text :address
-      t.integer :phone_number
+      t.string :address
+      t.string :phone_number
       t.string :category
 
       t.timestamps
